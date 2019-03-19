bcd7c61c50845c8aeccb5b0ae21f5e41be65a7dd
LeonLuna98 <conradolunap@gmail.com>
2019-03-19 16:17:31 -0300

    tp2

--- arreglo.cpp
+++ arreglo.cpp
@@ -11,9 +11,10 @@ int main(){
 		printf("El valor del elemento %d es de: %u\n", i, *(arre+i) );//Aritmetica de puntero
 		printf("La direccion de memoria del elemento %d es: %d\n", i, &arre[i]);//Notacion indexada
 		printf("El valor del elemento %d con puntero es de: %u\n", i, *(punt +i) );
-		printf("La direccion de memorioa del elemento %d con puntero es de: %u\n", i, &punt[i] );
-
+		printf("La direccion de memoria del elemento %d con puntero es de: %u\n", i, &punt[i] );
+		
 	}
-
+	printf("El tamanio del arreglo es: %d\n", sizeof(arre));
+	printf("El tamanio del puntero es: %d\n", sizeof(punt));
 return 0;
 }
