01fc9eed0a2d08a596916f76a08ca1158ceb1d17
LeonLuna98 <conradolunap@gmail.com>
2019-03-19 16:37:29 -0300

    orden de los puntos

--- arreglo.cpp
+++ arreglo.cpp
@@ -5,13 +5,13 @@ int main(){
 	int arre[5]={5,15,30,10,35};
 	int *punt;
 	punt= arre;
-	temp=punt;
+	//int temp=punt;
 	for(int i=0;i<5; i++ ){
 		
-		printf("El valor del elemento %d es de: %u\n", i, *(arre+i) );//Aritmetica de puntero
-		printf("La direccion de memoria del elemento %d es: %d\n", i, &arre[i]);//Notacion indexada
-		printf("El valor del elemento %d con puntero es de: %u\n", i, *(punt +i) );
-		printf("La direccion de memoria del elemento %d con puntero es de: %u\n", i, &punt[i] );
+		printf("La direccion de memoria del elemento %d es: %d\n", i, arre[i]);//Notacion indexada
+		printf("El valor del elemento %d con puntero es de: %d\n", i, *(punt +i));
+		printf("El valor del elemento %d es de: %d\n", i, *(arre+i) );//Aritmetica de puntero
+		printf("La direccion de memoria del elemento %d con puntero es de: %p\n", i, (punt +i));
 		
 	}
 	printf("El tamanio del arreglo es: %d\n", sizeof(arre));
